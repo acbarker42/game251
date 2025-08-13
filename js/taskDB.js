6a2f13b710206a12e54aa4210a752dcf39e4c080
Autumn Barker <autumn.c.barker@gmail.com>
2025-08-13 11:07:29 -0500

    imports, and choices widget
    created choices widget so only updating in one place
    the task db now is imported from a js file

---
+++ js/taskDB.js
@@ -0,0 +1,90 @@
+window.taskDBImport = [{
+        name: "CallMum",
+        text: "Call Your Mum",
+        time: 20,
+        timeLeft: 20,
+        visited: false,
+        status: "incomplete",
+        stimText: "You know you need to call your mom",
+        resolutionText: "Your mum is so happy you called.",
+        storyText: "You pick up the phone and speed dial your mom"
+
+    },
+    {
+        name: "FeedCats",
+        text: "Feed The Cats",
+        time: 5,
+        timeLeft: 5,
+        visited: false,
+        status: "incomplete",
+        stimText: "Your cats are rubbing up against your legs, a tell tale sign they are hungry",
+        resolutionText: "Your cats are so happy you fed them.",
+        storyText: "You go to feed the cats"
+    },
+    {
+        name: "TakeOutTrash",
+        text: "Take Out Trash",
+        time: 5,
+        timeLeft: 5,
+        visited: false,
+        status: "incomplete",
+        stimText: "You hear the trash truck down the street",
+        resolutionText: "The truck stops to pick up your neighbors trash.  Just in time!",
+        storyText: "You run to the garage to roll out the trash can"
+    },
+    {
+        name: "MakeBreakfast",
+        text: "Make Breakfast",
+        time: 10,
+        timeLeft: 10,
+        visited: false,
+        status: "incomplete",
+        stimText: "Your stomach is growling at you something fierce",
+        resolutionText: "Your belly is so happy its full.",
+        storyText: "You go to the kitchen to make breakfast"
+    },
+    {
+        name: "DoDishes",
+        text: "Do The Dishes",
+        time: 20,
+        timeLeft: 20,
+        visited: false,
+        status: "incomplete",
+        stimText: "You have no dishes that are clean",
+        resolutionText: "Your dishes are so happy their clean.",
+        storyText: "You grab you sponge to do the dishes"
+    },
+    {
+        name: "FillDishSoap",
+        text: "Fill Dish Soap",
+        time: 5,
+        timeLeft: 5,
+        visited: false,
+        status: "incomplete",
+        stimText: "You realize you are out of soap",
+        resolutionText: "Your soap is so happy to exist.",
+        storyText: "You go to refill your dish soap"
+    },
+    {
+        name: "GetGroceries",
+        text: "Get Groceries",
+        time: 60,
+        timeLeft: 60,
+        visited: false,
+        status: "incomplete",
+        stimText: "You realize you have no food",
+        resolutionText: "Your refrigerator is so happy to be full",
+        storyText: "You run to the store to buy groceries"
+    },
+    {
+        name: "PickUpTrash",
+        text: "Pick Up Trash",
+        time: 10,
+        timeLeft: 10,
+        visited: false,
+        status: "incomplete",
+        stimText: "You hear the cats knock over the trash can",
+        resolutionText: "Your trash can is so happy to be upright.",
+        storyText: "You go to clean up the mess"
+    },
+]
